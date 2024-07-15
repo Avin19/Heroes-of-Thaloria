cb1a5da7099e13ce9adc3517c0ce33f1e2d43687
Avin19 <avinashy1993@gmail.com>
2024-07-15 09:11:58 +0530

    Working On Animation

--- Assets/Project/Scripts/Player/PlayerMovemnt.cs
+++ Assets/Project/Scripts/Player/PlayerMovemnt.cs
@@ -2,24 +2,53 @@ using System;
 using UnityEngine;
 
 /// <summary>
-///  
+///  Player Movement 
 /// </summary>
-
-public class PlayerMovemnt : MonoBehaviour
+namespace Player.Movement
 {
-  [Header("Config")]
-  [SerializeField] private float moveSpeed;
 
-  private GameInput _gameInput;
-  private Rigidbody2D _rb;
-  private Vector2 _moveDir;
-
-  private void Awake()
+  public class PlayerMovemnt : MonoBehaviour
   {
-    _gameInput = new GameInput();
-    _rb = GetComponent<Rigidbody2D>();
-    
+    [Header("Config")] [SerializeField] private float _moveSpeed;
 
+    private GameInput _gameInput;
+    private Rigidbody2D _rb;
+    private Vector2 _moveDir;
+
+    private void Awake()
+    {
+      _gameInput = new GameInput();
+      _rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    private void FixedUpdate()
+    {
+      ReadMovement();
+      Move();
+    }
+
+    private void Move()
+    {
+      _rb.MovePosition(_rb.position + _moveDir * (_moveSpeed * Time.deltaTime));
+    }
+
+    private void ReadMovement()
+    {
+      _moveDir = _gameInput.Player.Move.ReadValue<Vector2>();
+    }
+
+
+
+    private void OnEnable()
+    {
+      _gameInput.Enable();
+    }
+
+    private void OnDisable()
+    {
+      _gameInput.Disable();
+    }
   }
 }
 
